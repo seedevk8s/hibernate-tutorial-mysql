1e5c3be17f635b4c0e29daf58852b9c7aab0035a
chjinlook <chjinlook@gmail.com>
2019-07-11 20:13:30 +0900

    기존 프로젝트 복사붙여넣기 및 환경체크

--- src/com/luv2code/jdbc/TestJdbc.java
+++ src/com/luv2code/jdbc/TestJdbc.java
@@ -7,10 +7,10 @@ public class TestJdbc {
 
 	public static void main(String[] args) {
 		
-		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?"
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_01_one_to_one_uni?"
 				+ "useSSL=false&serverTimezone=UTC";
-		String user = "hbstudent";
-		String pass = "hbstudent";
+		String user = "root";
+		String pass = "interhouse";
 		try {
 			System.out.println("Connecting to database: " + jdbcUrl);
 			
