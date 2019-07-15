44416bcaee8e0e9200a06e7f9ba4066aca773aa3
chjinlook <chjinlook@gmail.com>
2019-07-15 22:08:53 +0900

    get course for the instructor

---
+++ src/com/luv2code/hibernate/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,114 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorCoursesDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+
+			// create the objects
+			
+			// associate the objects
+			
+			
+			// start a transaction
+
+			
+			// save the instructor
+			/*
+			 * Note : this will ALSO save the details object
+			 * because of CascadeType.ALL
+			 */
+
+			
+			// commit transaction
+			
+			
+
+			
+			
+			session.beginTransaction();
+			
+			// get the instructor from db
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			System.out.println("Instructor: " + tempInstructor);
+			
+			// get course for the instructor
+			System.out.println("Courses: " + tempInstructor.getCourses());
+			
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
