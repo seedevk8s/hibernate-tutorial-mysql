9b25a6303209a93ab60b79f7cb609339fdfb91cd
chjinlook <chjinlook@gmail.com>
2019-07-16 11:28:35 +0900

    // option 2: Hibernate query with HQL

---
+++ src/com/luv2code/hibernate/demo/FetchJoinDemo.java
@@ -0,0 +1,134 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class FetchJoinDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+
+			// create the objects
+			
+			// associate the objects
+			
+			
+			// start a transaction
+
+			
+			// save the instructor
+			/*
+			 * Note : this will ALSO save the details object
+			 * because of CascadeType.ALL
+			 */
+
+			
+			// commit transaction
+			
+			
+
+			
+			
+			session.beginTransaction();
+			
+			// option 2: Hibernate query with HQL
+			
+			
+			// get the instructor from db
+			int theId = 1;
+
+			Query<Instructor> query = 
+					session.createQuery("select i from Instructor i "
+									+ "JOIN FETCH i.courses "
+									+ "where i.id=:theInstructorId", 
+							Instructor.class);
+			
+			// set parameter on query
+			query.setParameter("theInstructorId", theId);
+			
+			// execute query and get instructor
+			Instructor tempInstructor = query.getSingleResult();
+			
+			System.out.println("luv2code:>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Instructor: " + tempInstructor);		
+			
+			session.getTransaction().commit();
+			
+			// close the session
+			session.close();
+			
+			System.out.println("\nluv2code:>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> The session is now closed!\n ");
+			
+			// get course for the instructor
+			System.out.println("luv2code:>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Courses: " + tempInstructor.getCourses());
+			
+			
+			System.out.println("luv2code:>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
